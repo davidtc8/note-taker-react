0f63e4854b016a53ecab1ce80e723b9b2404816a
David Torres <davidtorresc8@gmail.com>
2022-11-09 11:12:18 -0600

    NotesListPage

---
+++ src/pages/NotesListPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import notes from '../assets/data'
+import Listitem from '../components/Listitem'
+
+const NotesListPage = () => {
+  return (
+    <div>
+      <div className= 'notes-list'>
+          {notes.map((note, index) => (
+              <Listitem key={index} note= {note}/>
+            ))}
+      </div>
+    </div>
+  )
+}
+
+export default NotesListPage
