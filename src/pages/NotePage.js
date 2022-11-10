bbf3482b26ad585329d35530c036684c0434562c
David Torres <davidtorresc8@gmail.com>
2022-11-10 16:45:53 -0600

    Pages for my application

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,25 +1,94 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, {useState, useEffect} from 'react'
+//import notes from '../assets/data'
 import { Link } from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 // As we added a number to our router, we also need to add a props
-const NotePage = ({match}) => {
+const NotePage = ({match, history}) => {
     let noteID = match.params.id
     // variable that matches the specific ID that we're passing in
-    let note = notes.find(note => note.id === Number(noteID))
+    //let note = notes.find(note => note.id === Number(noteID))
+    let [note, setNote] = useState(null)
+    useEffect(() => {
+        getNote()
+    }, [noteID])
+
+    let getNote = async() => {
+        if(noteID === 'new') return
+        let response = await fetch(`http://localhost:8000/notes/${noteID}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    let createNote = async() => {
+        await fetch(`http://localhost:8000/notes/`, {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date() })
+        })
+    }
+
+    // This will fire a put method on the backend, it'll send the object, therefore updating it
+    let updateNote = async() => {
+        await fetch(`http://localhost:8000/notes/${noteID}`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date() })
+        })
+    }
+
+    let deleteNote = async() => {
+        await fetch(`http://localhost:8000/notes/${noteID}`, {
+            method: 'DELETE',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({note})
+        })
+        history.push('/')
+    }
+
+    let handleSubmit = () => {
+        if(noteID !== 'new' && !note.body) {
+            deleteNote()
+        } else if(noteID !== 'new'){
+            updateNote()
+        } else if(noteID === 'new' && note !== null){
+            createNote()
+        }
+        updateNote()
+        history.push('/')
+    }
+
     console.log('Props', noteID)
     return (
         <div className='note'>
             <div className='note-header'>
                 <h3>
                     <Link to='/'>
-                        <ArrowLeft />
+                        <ArrowLeft onClick={handleSubmit} />
                     </Link>
                 </h3>
+
+                {/* If the noteID is not new */}
+                {/* If statement inside the react function */}
+                {noteID !== 'new' ? (
+                    <button onClick={deleteNote}>Delete</button>
+                ): (
+                    <button onClick={handleSubmit}>Done</button>
+                )}
             </div>
 
-            <textarea value={note?.body}>
+            {/* It needs to be like this, because, if you don't put the (e)
+                it'll trigger automatically, and we need for it to wait.
+                
+                We will also update the actual note, and more specifically the body
+                and the value to that body is e.target.value*/}
+            <textarea onChange={(e) => {setNote({...note, 'body': e.target.value})}} value={note?.body}>
 
             </textarea>
         </div>
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, { useState, useEffect } from 'react'
+//import notes from '../assets/data'
 import Listitem from '../components/Listitem'
+import AddButton from '../components/AddButton'
 
 const NotesListPage = () => {
+  // useState is going to return an array
+  let [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    getNotes()
+    // We're adding this array of dependencies at the end "[]"
+    // so that we don't have so many requests call in the website
+  }, [])
+
+  let getNotes = async()=> {
+    let response = await fetch('http://localhost:8000/notes/')
+    let data = await response.json()
+    setNotes(data)
+  }
+
   return (
     <div className= 'notes'>
       <div className= 'notes-header'>
@@ -17,6 +33,8 @@ const NotesListPage = () => {
               <Listitem key={index} note= {note}/>
             ))}
       </div>
+
+      <AddButton/>
     </div>
   )
 }
